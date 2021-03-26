bdb238a365b1ab8686ee68571391af607889a733
patrick <patrick@grupocriar>
2021-03-26 17:30:25 -0300

    refatoracao

--- app/Services/CriadorDeSerie.php
+++ app/Services/CriadorDeSerie.php
@@ -3,22 +3,30 @@
 namespace App\Services;
 
 use App\Serie;
+use Illuminate\Support\Facades\DB;
 
 class CriadorDeSerie {
 
     public function criarSerie (string $nomeSerie, int $qtdTemporada, int $epPorTemporada) : Serie {
-
+        DB::beginTransaction();
         $serie = Serie::create(['nome' => $nomeSerie]);
+        $this->criarTemporadas($qtdTemporada, $epPorTemporada, $serie);
+        DB::commit();
 
+        return $serie;
+    }
+
+    private function criarTemporadas (int $qtdTemporada, int $epPorTemporada, Serie $serie) : void {
         for ($i = 1; $i <= $qtdTemporada; $i++) {
             $temporada = $serie->temporadas()->create(['numero' => $i]);
 
-            for ($j = 1; $j <= $epPorTemporada; $j++) {
-                $temporada->episodios()->create(['numero' => $j]);
-            }
-
+            $this->criarEpisodios($epPorTemporada, $temporada);
         }
+    }
 
-        return $serie;
+    private function criarEpisodios (int $epPorTemporada, \Illuminate\Database\Eloquent\Model $temporada) : void {
+        for ($j = 1; $j <= $epPorTemporada; $j++) {
+            $temporada->episodios()->create(['numero' => $j]);
+        }
     }
 }
